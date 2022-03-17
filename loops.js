fa79ac3ac607726b7aa9d212f6d4abed399b2035
waynemorphic <wayneklyde@gmail.com>
2022-03-17 12:37:35 +0300

    Added a loop that returns even numbers only between 1 and 100

--- loops.js
+++ loops.js
@@ -6,3 +6,13 @@ for (i = 0; i <= 100; i++){
     numbers.push(i);
 }
 console.log(numbers);
+
+// loop that returns even numbers
+
+let evenNumbers = [];
+for (i = 0; i <= 100; i++){
+    if (i % 2 == 0){
+        evenNumbers.push(i);
+    }
+}
+console.log(evenNumbers);
